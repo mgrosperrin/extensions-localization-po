5197df0d35541d6e8677715e89f6ea0c471f3f06
Matthias GROSPERRIN <matthias.grosperrin@gmail.com>
2025-08-25 10:56:42 +0200

    Add tests

---
+++ tests/MGR.Extensions.Localization.PortableObject.UnitTests/PortableObjectLocalizationOptionsTests.SetResourcesFolder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MGR.Extensions.Localization.PortableObject.UnitTests;
+public partial class PortableObjectLocalizationOptionsTests
+{
+    public class SetResourcesFolder
+    {
+        [Fact]
+        public void GivenEmptyResourcesFolder_WhenSettingResourcesFolder_ThenShouldSetResourcesFolder()
+        {
+            // Arrange
+            var options = new PortableObjectLocalizationOptions();
+            
+            // Act
+            options.SetResourcesFolder(string.Empty);
+            
+            // Assert
+            Assert.Equal(string.Empty, options.ResourcesFolder);
+        }
+        [Fact]
+        public void GivenValidResourcesFolder_WhenSettingResourcesFolder_ThenShouldSetResourcesFolder()
+        {
+            // Arrange
+            var options = new PortableObjectLocalizationOptions();
+            var resourcesFolder = "MyResources";
+            
+            // Act
+            options.SetResourcesFolder(resourcesFolder);
+
+            // Assert
+            Assert.Equal("MyResources", options.ResourcesFolder);
+        }
+    }
+}
